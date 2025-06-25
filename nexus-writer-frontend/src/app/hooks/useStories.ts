d1a45be06943d8d346e357fc2d814940d4f8cfde
AbdullaK123 <abdullamaseeh123@gmail.com>
2025-06-25 17:26:04 +0400

    added useStories hook

---
+++ nexus-writer-frontend/src/app/hooks/useStories.ts
@@ -0,0 +1,108 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { StoryCreateRequest, StoryUpdateRequest } from "../types/stories"
+
+
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_DOMAIN
+
+
+export function useStories() {
+
+    const queryClient = useQueryClient()
+
+    // getting all stories for a user
+    const { 
+        data: stories, 
+        isLoading, 
+        isError, 
+        isSuccess 
+    } = useQuery({
+        queryKey: ['stories'],
+        queryFn: () => fetch(`${API_URL}/stories`, {
+            credentials: 'include'
+        }).then((res) => {
+            if (!res.ok) throw new Error("Failed to fetch stories")
+            return res.json()
+        })
+    })
+
+    // single story fetch
+    const getStory = (storyId: string) => useQuery({
+        queryKey: ['stories', storyId],
+        queryFn: () => fetch(`${API_URL}/stories/${storyId}`, {
+            credentials: 'include'
+        }).then((res) => {
+            if (!res.ok) throw new Error("Failed to fetch story")
+            return res.json()
+        })
+    })
+
+
+    // post mutation
+    const createStoryMutation = useMutation({
+        mutationFn: (storyInfo : StoryCreateRequest) => fetch(`${API_URL}/stories`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(storyInfo)
+        }).then((res) => {
+            if (!res.ok) throw new Error('Failed to create story')
+            return res.json()
+        }),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['stories'] })
+    })
+
+
+    // delete mutation
+    const deleteStoryMutation = useMutation({
+        mutationFn: (storyId : string) => fetch(`${API_URL}/stories/${storyId}`, {
+            method: 'DELETE',
+            credentials: 'include'
+        }).then((res) => {
+            if (!res.ok) throw new Error('Failed to delete story')
+            return res.json()
+        }),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['stories'] })
+    })
+
+
+    // put mutation
+    const updateStoryMutation = useMutation({
+        mutationFn: ({ body, storyId } : StoryUpdateRequest) => fetch(`${API_URL}/stories/${storyId}`, {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then((res) => {
+            if (!res.ok) throw new Error('Failed to update story')
+            return res.json()
+        }),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['stories'] })
+    })
+
+
+    // return everything
+    return {
+        stories,
+        getStory,
+        create: createStoryMutation.mutate,
+        update: updateStoryMutation.mutate,
+        delete: deleteStoryMutation.mutate,
+        isLoading,
+        isCreating: createStoryMutation.isPending,
+        isUpdating: updateStoryMutation.isPending,
+        isDeleting: deleteStoryMutation.isPending,
+        isError,
+        creationError: createStoryMutation.isError,
+        updateError: updateStoryMutation.isError,
+        deleteError: deleteStoryMutation.isError,
+        isSuccess,
+        isCreated: createStoryMutation.isSuccess,
+        isUpated: updateStoryMutation.isSuccess,
+        isDeleted: deleteStoryMutation.isSuccess
+    }
+
+}
--- nexus-writer-frontend/src/app/types/stories.ts
+++ nexus-writer-frontend/src/app/types/stories.ts
@@ -4,3 +4,13 @@ export type StoryCreateRequest = {
     title: string;
 }
 
+export type StoryUpdateRequestBody = {
+    title?: string;
+    status?: 'Complete' | 'Ongoing' | 'On Haitus'
+}
+
+export type StoryUpdateRequest = {
+    body?: StoryUpdateRequestBody;
+    storyId: string;
+}
+
